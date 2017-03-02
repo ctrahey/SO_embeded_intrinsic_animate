74129b94a2d298f0f0a78a3a5cf0592674ebf732
Christopher Trahey <christrahey@gmail.com>
2017-03-02 14:45:21 -0800

    Initial state

---
+++ constraint animation/InnerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InnerViewController.swift
+//  constraint animation
+//
+//  Created by Christopher Trahey on 3/2/17.
+//  Copyright © 2017 trahey. All rights reserved.
+//
+
+import UIKit
+
+class InnerViewController: UIViewController {
+    @IBOutlet weak var innerViewHeightConstraint: NSLayoutConstraint!
+
+    var isCollapsed = false {
+        didSet {
+            let factor:CGFloat = isCollapsed ? 1.5 : 0.66
+            let existing = innerViewHeightConstraint.constant
+            UIView.animate(withDuration: 1.0) {
+                self.innerViewHeightConstraint.constant = existing * factor
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+
+    @IBAction func pokeTapped(_ sender: UIButton) {
+        isCollapsed = !isCollapsed
+    }
+
+}
