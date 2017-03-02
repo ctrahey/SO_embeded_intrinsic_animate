d1ea5957f048032a7495e457bac5a5b460d56bbd
Christopher Trahey <christrahey@gmail.com>
2017-03-02 13:58:11 -0800

    Initial Commit

---
+++ constraint animation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  constraint animation
+//
+//  Created by Christopher Trahey on 3/2/17.
+//  Copyright © 2017 trahey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
